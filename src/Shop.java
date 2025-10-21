716a3b7b0873f5dbbbcf59fd927cf3e8d0e9d32d
mihai <mihai2005.dreghiciu@gmail.com>
2025-10-21 16:14:31 +0300

    4.2

--- src/Shop.java
+++ src/Shop.java
@@ -11,4 +11,18 @@ public class Shop {
         return min;
     }
 
+    public static int mostExpensiveItem(int[] keyboards, int[] usbs) {
+        int max = keyboards[0];
+
+        for (int k : keyboards)
+            if (k > max)
+                max = k;
+
+        for (int u : usbs)
+            if (u > max)
+                max = u;
+
+        return max;
+    }
+
 }
