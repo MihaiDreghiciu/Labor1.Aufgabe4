2dd010586a2072a3db9a5af32a5f21ee33fe9a42
mihai <mihai2005.dreghiciu@gmail.com>
2025-10-21 16:14:02 +0300

    4.1

--- src/Shop.java
+++ src/Shop.java
@@ -1,2 +1,14 @@
 public class Shop {
+
+    public static int cheapestKeyboard(int[] keyboards) {
+        int min = keyboards[0];
+
+        for (int k : keyboards) {
+            if (k < min)
+                min = k;
+        }
+
+        return min;
+    }
+
 }
